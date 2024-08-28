f0cc246fa1eda7fa9b6038e2f79ffbb6ccbb6762
ndlanzi <2451244@students.wits.ac.za>
2024-08-28 23:22:45 +0200

    Add build in package.json

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/auth/google/callback'
+    callbackURL:  process.env.GOOGLE_CALLBACK_URL || 'http://localhost:3000/auth/google/callback'
   },
   function(accessToken, refreshToken, profile, done) {
     // Check if the user already exists in the database
